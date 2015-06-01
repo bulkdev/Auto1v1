b91a714777276ba163fa641cb14fd18de5da0e67
Jayden Patrick <jaydenpatrick1110@gmail.com>
2015-06-01 17:09:56 -0400

    Create Provide.php

---
+++ src/Auto1v1/ItzBulkDev/Provider/Provide.php
@@ -0,0 +1,50 @@
+<?php
+namespace <?php
+namespace Auto1v1\Provider;
+use Auto1v1\Main;
+interface Provider{
+    /**
+     * Initialize Provider
+     * @param TouchIt $plugin
+     */
+    public function __construct(TouchIt $plugin);
+    /**
+     * Call when plugin disable or manually save
+     */
+    public function save();
+    /**
+     * @param $x
+     * @param $y
+     * @param $z
+     * @param $level
+     * @return array
+     */
+    public function get($x, $y, $z, $level);
+    /**
+     * @param array $data
+     * @param $x
+     * @param $y
+     * @param $z
+     * @param $level
+     */
+    public function create(array $data, $x, $y, $z, $level);
+    /**
+     * @param $x
+     * @param $y
+     * @param $z
+     * @param $level
+     */
+    public function remove($x, $y, $z, $level);
+    /**
+     * @param $x
+     * @param $y
+     * @param $z
+     * @param $level
+     * @return bool
+     */
+    public function exists($x, $y, $z, $level);
+    /**
+     * @return array
+     */
+    public function getAll();
+}
